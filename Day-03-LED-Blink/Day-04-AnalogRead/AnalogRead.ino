b0eece7d30c3d0e53640cea189900406cd4926e5
nallellavasanthkumar <nallellavasanthkumar@gmail.com>
2026-07-28 21:59:23 +0530

    Update AnalogRead.ino

--- Day-03-LED-Blink/Day-04-AnalogRead/AnalogRead.ino
+++ Day-03-LED-Blink/Day-04-AnalogRead/AnalogRead.ino
@@ -1,1 +1,24 @@
+/*WAP to read val from POT and print it on serial monitor*/
 
+void setup()
+{
+  Serial.begin(9600);
+}
+
+unsigned int sensor_val;
+unsigned int map_val;
+
+void loop()
+{
+  sensor_val = analogRead(A0);
+
+  Serial.print("analog val is ");
+  Serial.println(sensor_val);
+
+  map_val = map(sensor_val, 0, 1023, 0, 100);
+
+  Serial.print("maped val is ");
+  Serial.println(map_val);
+
+  delay(1000);
+}
