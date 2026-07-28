29f5caa8c0f782dbeffeda899ea4527e4fbd9d9a
nallellavasanthkumar <nallellavasanthkumar@gmail.com>
2026-07-28 22:19:09 +0530

    Create LED_Blink.ino

---
+++ Day-03-LED-Blink/LED_Blink.ino
@@ -0,0 +1,24 @@
+/*
+ * Project : Smart Factory Monitoring System
+ * Module  : Day 03 - LED Blink Test
+ * Author  : Nallella Vasanth Kumar
+ * Description:
+ * This program blinks the onboard LED connected to pin 13
+ * to verify the Arduino UNO board and digital output.
+ */
+
+#define LED_PIN 13
+
+void setup()
+{
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
+}
